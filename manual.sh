4388176b6902f31263ecc87a798d45bfaffa1c13
Mithun Technologies <kumarbharath973554@gmail.com>
2022-07-30 19:13:21 +0530

    master

---
+++ manual.sh
@@ -0,0 +1,14 @@
+good evening guys
+hi to all
+welcome to sathvik technologies
+welcome to manual testing class
+hope you will enjoy the class
+all the best 
+
+
+
+enjoy the classes
+this is the best institute
+all the best 
+thankyou
+
