98a6b72016d05bfd5d47593ca1cdbe51c0039ebb
Mithun Technologies <kumarbharath973554@gmail.com>
2022-07-30 18:04:19 +0530

    modified testing

--- testing.sh
+++ testing.sh
@@ -1,4 +1,8 @@
+good evening guys 
+this is the first class on testing
 hi guys welcome to testing classes
 hope you will enjoy the session
 all the best
 thankyou
+enjoy the classes 
+listen carefully
